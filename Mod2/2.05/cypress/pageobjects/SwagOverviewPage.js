aa4108975e29f8ba69d8fbc9002f8075205e8d02
Lindsay Walker <lindsay.waklker@saucelabs.com>
2021-04-19 18:56:57 -0500

    re-did 2.05

--- Mod2/2.05/cypress/pageobjects/SwagOverviewPage.js
+++ Mod2/2.05/cypress/pageobjects/SwagOverviewPage.js
@@ -1,9 +1,6 @@
-//filename: cypress/pageobjects/SwagOverviewPage.js
 class SwagOverviewPage {
-    // Make it private so people can't mess with it
-    // Source: https://github.com/tc39/proposal-class-fields#private-fields
-    get screen() {
-        return cy.get('.inventory_list');
-    }
+  get screen() {
+    return cy.get('.inventory_list')
+  }
 }
 export default new SwagOverviewPage();
--- Mod2/2.06/cypress/pageobjects/SwagOverviewPage.js
+++
@@ -1,6 +0,0 @@
-class SwagOverviewPage {
-  get screen() {
-    return cy.get('.inventory_list')
-  }
-}
-export default new SwagOverviewPage();
